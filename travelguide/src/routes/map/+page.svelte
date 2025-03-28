b0fa42a34553a71a12b22bad42bb1cc2bb086a28
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-03-28 11:01:39 +0100

    map

---
+++ travelguide/src/routes/map/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import maplibregl from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import { onMount } from 'svelte';
+
+	let mapDiv : HTMLDivElement;
+
+	onMount(() => {
+		const map = new maplibregl.Map({
+			container: mapDiv, // container id
+			style: 'https://demotiles.maplibre.org/style.json', // style URL
+			center: [0, 0], // starting position [lng, lat]
+			zoom: 1 // starting zoom
+		});
+	});
+</script>
+
+<div bind:this={mapDiv} id="map" class="h-dvh"></div>
