9253b4d0efdc44ded0d1858bcb791a16369a3585
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-04-02 16:01:10 +0200

    sqlite for now

--- travelguide/src/routes/+page.svelte
+++ travelguide/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	export let data: any;
+</script>
+
+<div>
+	<h1>My Blog</h1>
+
+	<main>
+		<div>
+			{#each data.feed as post (post.id)}
+				post
+			{/each}
+		</div>
+	</main>
+</div>
