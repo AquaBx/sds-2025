4eb6a8f7d431270bac2651c16f4132e2e63b8725
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-04-09 11:21:33 +0200

    Database (#29)
    * added new models
    
    * simplify
    
    * integration in svelte
    
    * update npm packages

--- travelguide/src/routes/+page.server.ts
+++ travelguide/src/routes/+page.server.ts
@@ -1,12 +1,20 @@
 import prisma from '$lib/prisma';
 
-export const load = (async () => {
+export const load = async () => {
 	// 1.
 
-	const response = await prisma.museum.findMany({
+	const response = await prisma.activity.findMany({
+		where: {
+			score: {
+				gte: 3 // Example condition for filtering by score
+			}
+		},
+		include: {
+			picture: true // Include related pictures
+		}
 	});
 
 	// 2.
 
 	return { feed: response };
-}) ;
+};
