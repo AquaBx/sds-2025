ae11a8735fa7bc53190e6cb73cd190e32402b2c5
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-11 15:05:34 +0200

    Search by name (#50)
    * Added SearchBar + searchByName function
    
    * Added search by theme feature
    
    * Added search by theme feature
    
    Co-authored-by: fquinaou <francois.quinaou@etudiant.univ-rennes1.fr>

---
+++ travelguide/src/lib/components/SearchBar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { searchPlaces }: { searchPlaces: Function } = $props();
+	let value: string = $state('');
+</script>
+
+<div class="m-2">
+	<input
+		type="text"
+		placeholder="Search for a place (name, theme, etc...)"
+		class="input w-full"
+		bind:value
+		oninput={() => searchPlaces(value)}
+	/>
+</div>
