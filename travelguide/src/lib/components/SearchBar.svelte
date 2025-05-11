35a402eb6f80f6881de0ebe98e508e4314343359
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-11 15:01:01 +0200

    simplify

--- travelguide/src/lib/components/SearchBar.svelte
+++ travelguide/src/lib/components/SearchBar.svelte
@@ -1,23 +1,14 @@
 <script lang="ts">
-	let { searchPlaces } = $props();
+	let { searchPlaces }: { searchPlaces: Function } = $props();
 	let value: string = $state('');
-	
-	import type { Place } from '$lib/types';
-	import { MagnifyingGlass } from '@steeze-ui/phosphor-icons';
-
-
 </script>
+
 <div class="m-2">
-	
 	<input
 		type="text"
 		placeholder="Search for a place (name, theme, etc...)"
-		class="input w-full" bind:value={value}
-		oninput={(e) => {
-			e.preventDefault();
-			searchPlaces(value);
-		}}
-		
-		/>
+		class="input w-full"
+		bind:value
+		oninput={() => searchPlaces(value)}
+	/>
 </div>
-
