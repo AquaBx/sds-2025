7deffbae6d2a804b33b516ef50fc0dfd55a98576
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-21 23:05:16 +0200

    Frontend rework (#63)
    * start
    
    * refacto
    
    * fixes
    
    * globalstate
    
    * ok

--- travelguide/src/routes/api/cities/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json } from '@sveltejs/kit';
-import PocketBase from 'pocketbase';
-
-const pb = new PocketBase(process.env.DATABASE);
-
-export const GET = async () => {
-  const cities = await pb.collection('cities').getFullList();
-  return json({ cities: cities });
-};
