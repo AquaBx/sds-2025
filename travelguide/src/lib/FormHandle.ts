0f5b7d2c9e56ca61ee32703bba823443a7d0f269
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-25 23:04:00 +0200

    Agent based (#65)
    * wip
    
    * fix build + cleanup
    
    * some fixes

--- travelguide/src/lib/FormHandle.ts
+++ travelguide/src/lib/FormHandle.ts
@@ -1,25 +1,24 @@
 import { toast } from "svelte-sonner";
 import { config } from "./store.svelte";
 
-export const handler = ({
-    formElement,
-    formData,
-    action,
-    cancel,
-    submitter,
-    controller,
-}: {
+type HandlerInput = {
     action: URL;
     formData: FormData;
     formElement: HTMLFormElement;
     controller: AbortController;
     submitter: HTMLElement | null;
     cancel: () => void;
-}) => {
+}
+
+export function handler({ formElement, formData, act, cancel, submitter, controller }: HandlerInput) {
     return async ({ result, update }) => {
         if (result.type === 'success' && result.data?.activities) {
             config.activities = result.data.activities;
             config.hasSearched = true;
+        }
+        else if (result.type === 'success' && result.data?.itinerary) {
+            config.itinerary = result.data.itinerary;
+            config.hasItinerate = true;
         } else if (result.type === 'error') {
             toast.error('Form submission failed', {
                 description: result.error?.message || 'Please try again'
