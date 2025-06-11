82c63d2e4a83310351d7f9d6a444709dbace14ea
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-06-11 19:41:13 +0200

    Itinary (#67)
    * wip
    
    * route is appearing
    
    * wip
    
    * ok
    
    * Update routes.bru

--- travelguide/src/lib/components/PlaceCard.svelte
+++ travelguide/src/lib/components/PlaceCard.svelte
@@ -1,35 +1,42 @@
 <script lang="ts">
 	import type { Place } from '$lib/types';
-	import { HourglassMedium, Wheelchair } from '@steeze-ui/phosphor-icons';
+	import { DoorOpen, HourglassMedium, Wheelchair } from '@steeze-ui/phosphor-icons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	export let place: Place;
+
+	import * as Card from '$lib/components/ui/card/index.js';
 </script>
 
-<div class="flex flex-col gap-4 rounded-lg border border-gray-300 p-4">
-	<img
-		class="object-cover aspect-video w-full cursor-pointer rounded-lg transition-transform duration-300 ease-in-out hover:scale-105"
-		src={place.picture}
-		alt={place.name}
-	/>
-	<div class="flex flex-1 flex-col justify-center">
-		<div class="mb-1 text-2xl font-semibold">{place.name}</div>
-		<div class="flex flex-1 flex-row">
-			<div class="mb-2 text-base text-gray-600">{place.theme}</div>
+<Card.Root class="w-full max-w-sm">
+	<Card.Header>
+		<Card.Title>{place.name}</Card.Title>
+		<Card.Description>
+			{place.description}
+		</Card.Description>
+		<!-- <Card.Action>
+		</Card.Action> -->
+	</Card.Header>
+	<Card.Content>
+		<img
+			class="object-cover aspect-video w-full cursor-pointer rounded-lg"
+			src={place.picture}
+			alt={place.name}
+		/>
+		<div class="flex flex-1 flex-row gap-1">
+			<!-- <div class="mb-2 text-base">{place.type}</div> -->
 			{#if place.disableAccessibility}
-				<div class="mb-2 ml-2 flex items-center gap-1 text-sm font-semibold">
+				<div class="flex items-center gap-1 text-sm font-semibold">
 					<Icon src={Wheelchair} theme="solid" class="color-gray-900 size-4" />
 				</div>
 			{/if}
-			<div
-				class="mb-2 {place.disableAccessibility
-					? 'ml-2'
-					: 'ml-auto'} flex items-center gap-1 text-sm font-semibold"
-			>
+			<div class="flex items-center gap-1 text-sm font-semibold">
 				<Icon src={HourglassMedium} theme="solid" class="color-gray-900 size-4" />
 				<p>{place.estimatedDuration} {place.estimatedDuration <= 1 ? 'hour' : 'hours'}</p>
 			</div>
+			<div class="flex items-center gap-1 text-sm font-semibold">
+				<Icon src={DoorOpen} theme="solid" class="color-gray-900 size-4" />
+				<p>{place.openingTime}</p>
+			</div>
 		</div>
-		<div class="text-sm leading-relaxed text-gray-800">{place.description}</div>
-		<div>Open at : {place.openingTime}</div>
-	</div>
-</div>
+	</Card.Content>
+</Card.Root>
